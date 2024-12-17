3be8346c4cc9009dcd5c00ed5bd781da6afd9c26
claires67 <clairgethick@gmail.com>
2024-12-17 15:22:10 +0700

    Update service.py

--- service.py
+++ service.py
@@ -22,7 +22,7 @@ to celebrate what is being hailed as 'The Leap of the Century."
 class Summarization:
     def __init__(self) -> None:
         # Load model into pipeline
-        self.pipeline = pipeline('summarization2')
+        self.pipeline = pipeline('summarization')
     
     @bentoml.api
     def summarize(self, text: str = EXAMPLE_INPUT) -> str:
